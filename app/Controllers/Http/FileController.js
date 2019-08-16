4771b10e378c73ee79fb47abbbe5253a7fe1338f
Gabriel Monteiro Fernandes <gamofe@protonmail.ch>
2019-08-16 17:08:46 -0300

    Upload de arquivos

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const File = use('App/Models/File')
+const Helpers = use('Helpers')
+
+class FileController {
+  async store ({ request, response }) {
+    try {
+      if (!request.file('file')) return
+
+      const upload = request.file('file', { size: '5mb' })
+      const name = `${Date.now()}.${upload.subtype}`
+
+      await upload.move(Helpers.tmpPath('uploads'), { name })
+
+      if (!upload.moved()) {
+        throw upload.error()
+      }
+
+      const file = await File.create({
+        file: name,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype
+      })
+
+      return file
+    } catch (error) {
+      return response.status(error.status).send({ error: { message: error.message } })
+    }
+  }
+}
+
+module.exports = FileController
