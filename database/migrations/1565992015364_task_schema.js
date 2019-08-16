4eb800d1e379cb4cdc058bf3427c881b8529e66b
Gabriel Monteiro Fernandes <gamofe@protonmail.ch>
2019-08-16 19:24:05 -0300

    Criando models de projetos/tarefas

---
+++ database/migrations/1565992015364_task_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class TaskSchema extends Schema {
+  up () {
+    this.create('tasks', (table) => {
+      table.increments()
+      table.integer('project_id').unsigned().references('id').inTable('projects').onUpdate('CASCADE').onDelete('CASCADE')
+      table.integer('user_id').unsigned().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
+      table.integer('file_id').unsigned().references('id').inTable('files').onUpdate('CASCADE').onDelete('CASCADE')
+      table.string('title').notNullable()
+      table.text('description')
+      table.timestamp('due_date')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('tasks')
+  }
+}
+
+module.exports = TaskSchema
